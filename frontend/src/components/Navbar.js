9fc38edf6495ad1e9de1ac97ebab297e8d31c6b9
zer0000x <te.we@outlook.sa>
2025-02-11 15:47:01 +0200

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 text-white p-4">
+      <h1 className="text-lg font-bold">🚀 تطبيق الإشعارات</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
